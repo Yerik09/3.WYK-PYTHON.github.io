f92f5525ca11907e2bde0431dea3c0a9adb9a329
Yerik09 <hurtadoyerik@gmail.com>
2026-06-18 10:16:12 -0500

    update for hosting

--- proyecto_wyk/config/settings.py
+++ proyecto_wyk/config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Cargar las variables del archivo .env
 load_dotenv()
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 # Application definition
 
@@ -81,15 +82,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        engine='django.db.backends.postgresql'  # <-- Con la "q" al final
+    )
+}
+
+# Si estás en tu PC local (donde no existe DATABASE_URL), usará tus variables del .env
+if not os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',  # <-- Con la "q" al final
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
-}
 
 AUTH_USER_MODEL = 'usuarios.Usuario'
 
@@ -134,6 +143,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # --- CONFIGURACIÓN DE ARCHIVOS MULTIMEDIA (IMÁGENES) ---
 # URL para acceder a las imágenes desde el navegador
